0c0e1a6d72cdf48f8761157a013ee667f0d14c2a
Tagre <mael.clouet@hotmail.fr>
2017-11-16 17:50:03 +0100

    interface en javafx

---
+++ src/Hackathon.java
@@ -0,0 +1,39 @@
+
+
+
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+/**
+ *
+ * @author payen8u
+ */
+public class Hackathon extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("hackathon.fxml"));
+        
+        Scene scene = new Scene(root, 1000, 800);
+        
+        stage.setTitle("Big Graph Data");
+        stage.setScene(scene);
+        
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/HackathonController.java
@@ -0,0 +1,89 @@
+
+
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+/**
+ *
+ * @author payen8u
+ */
+public class HackathonController implements Initializable{
+    
+    @FXML
+    WebView webView;
+    private WebEngine webEngine;
+    private TextField textField;
+    public ListView listeColonnes;
+    public Text champLabel;
+    public ObservableList<Integer> selectedIndices;
+    //public LectureData lect=new LectureData("data-train.tab");
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        webEngine = webView.getEngine();
+        File f = new File("Vizu.html");
+         try { 
+         webEngine.load(f.toURI().toURL().toString());
+         } catch (MalformedURLException ex){
+             System.out.println("Impossible");
+         }
+        
+        listeColonnes.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        
+        BufferedReader flotFiltre;
+		String filtre;
+		Scanner scannerl;
+		
+		try {
+			flotFiltre = new BufferedReader(new FileReader("data-train.tab"));
+			filtre=flotFiltre.readLine();
+			String[] elements=filtre.split("\t");
+			ObservableList<String> items =FXCollections.observableArrayList ();
+	        for (String element:elements){
+	            items.add(element);
+	        }
+	        listeColonnes.setItems(items);
+	        flotFiltre.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        
+    }
+    
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        champLabel.setText(textField.getText());
+    }
+    
+    @FXML
+    protected void handleValidateButtonAction(ActionEvent event) {
+        selectedIndices = listeColonnes.getSelectionModel().getSelectedIndices();
+    }
+}
